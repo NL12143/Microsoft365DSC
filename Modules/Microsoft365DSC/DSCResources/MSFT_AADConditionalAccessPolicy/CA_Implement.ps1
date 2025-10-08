09de1980ef184fff8f296cb3831c4a0dcc5525d1
Roeland Cerfonteijn <NL12143@hotmail.com>
2025-10-08 15:08:25 +0200

    Update CA_Implement.ps1

--- Modules/Microsoft365DSC/DSCResources/MSFT_AADConditionalAccessPolicy/CA_Implement.ps1
+++ Modules/Microsoft365DSC/DSCResources/MSFT_AADConditionalAccessPolicy/CA_Implement.ps1
@@ -4,6 +4,9 @@ Configure-CA.ps1
 
 # COMPILE to MOF
 .\Configure-CA.ps1
+Executes the Configuration block as a function
+Creates a folder named ConditionalAccessPolicyConfig
+Inside that folder, generates localhost.mof
 
 # APPLY CALL like FUNCTION
 Start-DscConfiguration -Path .\ConditionalAccessPolicyConfig -Wait -Verbose -Force
